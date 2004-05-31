070f606cb2a520ceeb6669f8986713fb495c388f
mark <mark>
2004-05-31 14:21:02 +0000

    Added support for shell script as part of test suite

--- Testcases/run_test.sh
+++ Testcases/run_test.sh
@@ -3,6 +3,7 @@ if [ ! -d $1 ]; then
 	echo $0: missing testcase directory
 	exit 1
 fi
+# check for bt script
 if [ -r $1/script ]; then
 	cd $1
 	if [ "$2" = "create_output_masters" ]; then
@@ -16,6 +17,20 @@ if [ -r $1/script ]; then
 			echo -e "$1: \tFAILED <--------------------------"
 		fi
 	fi
+# check for shell script
+elif [ -r $1/script.sh ]; then
+	cd $1
+	if [ "$2" = "create_output_masters" ]; then
+		sh ./script.sh >output_master 2>&1
+	else
+		sh ./script.sh >output_test 2>&1
+		diff output_master output_test >/dev/null 2>&1
+		if [ $? -eq 0 ]; then
+			echo -e "$1: \tpassed"
+		else
+			echo -e "$1: \tFAILED <--------------------------"
+		fi    
+    fi
 else
 	echo -e "$1: \tno script file"
 fi
