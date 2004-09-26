63638184306fcb98fbb9fd36d8ee330f076959da
mark <mark>
2004-09-26 11:37:17 +0000

    Add cvs id and GNU licence text

--- inc/bc.h
+++ inc/bc.h
@@ -1,9 +1,29 @@
+/*
+ * $id$
+ *
+ * This file is part of the B Tree library.
+ *
+ * The B Tree library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * The B Tree library  is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #ifndef _btconst_
 #define _btconst_
 
 /*
-   B tree constants
-*/
+ *  B tree constants
+ */
 
 
 #define DEBUG 0
@@ -148,7 +168,6 @@
 #define QDELCR  28
 
 #define QBADIX  29
-
 #define QNOBTF  30
 #define QINERR  31
 #define QBADOP  32
